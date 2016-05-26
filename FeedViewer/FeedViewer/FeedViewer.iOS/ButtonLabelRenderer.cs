f7a0442c9bea160457fb6d4ad0ef30f5e8daf5ec
Gyuwon Yi <gyuwon@live.com>
2016-05-27 00:21:50 +0900

    ButtonLabel 컨트롤을 추가하고 사용자 이름에 적용합니다.

---
+++ FeedViewer/FeedViewer/FeedViewer.iOS/ButtonLabelRenderer.cs
@@ -0,0 +1,38 @@
+﻿using FeedViewer;
+using FeedViewer.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ButtonLabel), typeof(ButtonLabelRenderer))]
+
+namespace FeedViewer.iOS
+{
+    public class ButtonLabelRenderer : LabelRenderer
+    {
+        public ButtonLabelRenderer()
+        {
+            AddGestureRecognizer(new UITapGestureRecognizer(OnTapped));
+        }
+
+        private void OnTapped()
+        {
+            var element = Element as ButtonLabel;
+            element?.RaiseTapped();
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            Label element = e.NewElement;
+            UILabel control = Control;
+            if (element != null && control != null)
+            {
+                control.AttributedText = new NSAttributedString(
+                    element.Text, underlineStyle: NSUnderlineStyle.Single);
+            }
+        }
+    }
+}
---
+++ FeedViewer/FeedViewer/FeedViewer/ButtonLabel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FeedViewer
+{
+    public class ButtonLabel : Label
+    {
+        public event EventHandler Tapped;
+
+        public void RaiseTapped() => Tapped?.Invoke(this, EventArgs.Empty);
+    }
+}
