5e2de9fe8db685f42ab0f71f21045955bdc80a2b
Gyuwon Yi <gyuwon@live.com>
2016-05-26 20:28:45 +0900

    솔루션을 초기화합니다.

---
+++ FeedViewer/FeedViewer/FeedViewer.iOS/AppDelegate.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace FeedViewer.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
